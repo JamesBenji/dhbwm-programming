9af64c8fe3988b08159113e89d640d61c1d82094
JamesBenji <jamesbenjamin450@gmail.com>
2026-01-15 14:12:55 +0100

    Reading user input

--- java-steps/4. Data Types/DataTypes.java
+++ java-steps/4. Data Types/DataTypes.java
@@ -12,9 +12,9 @@ public class DataTypes {
         byte    tinyNum = 127;                  // 8-bit     from -128           to 127
         short   smallNum = 32000;               // 16-bit    from -32,768        to 32,767
         int     integer = 1;                    // 32-bit    from -2^31          to (2^31) - 1
-        long    longNumber = 1_000_000_000L;    // 64-bit    from -2^63          to (2^63) - 1
+        long    longNumber = 1_000_000_000L;    // 64-bit    from -2^63          to (2^63) - 1              The L is important. All long numbers must have it.
 
-        float   decimal = 1.1F;                 // 32-bit    from 1.4 * 10^-45   to approx. 3.4 * 10^38
+        float   decimal = 1.1F;                 // 32-bit    from 1.4 * 10^-45   to approx. 3.4 * 10^38     The F is important. All float numbers must have it.
         double  doubleDecimal = 1.1;            // 64-bit    from 4.9 * 10^-324  to approx. 1.7977 * 10^308
 
         char    character = 'c';                // 16-bit    from '\u0000'       to '\uffff'

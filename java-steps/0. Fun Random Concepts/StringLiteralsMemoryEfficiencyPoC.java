0b18edb9fd5c1a71e115787025c172edc65befda
JamesBenji <jamesbenjamin450@gmail.com>
2026-01-27 13:52:41 +0100

    Value comparison in the string reference type

--- java-steps/0. Fun Random Concepts/StringLiteralsMemoryEfficiencyPoC.java
+++ java-steps/0. Fun Random Concepts/StringLiteralsMemoryEfficiencyPoC.java
@@ -1,7 +1,8 @@
 public class StringLiteralsMemoryEfficiencyPoC {
     public static void main(String[] args) {
         System.out.println("We will use the same literal value to illustrate the different workings of the two ways of creating string literals.");
-        System.out.println("String string1 = ... will share the memory of the literal with the other variable attempting to create it. The comparison results in true because the variable point to exactly the same memory location despite the values being the same. The same test fails with new String()");
+        System.out.print("String string1 = ... will share the memory of the literal with the other variable attempting to create it. ");
+        System.out.println("The comparison results in true because the variable point to exactly the same memory location despite the values being the same. The same test fails with new String()");
         
         System.out.println("----------------------------------------------------------------------");
 
@@ -20,5 +21,13 @@ public class StringLiteralsMemoryEfficiencyPoC {
         String string4 = new String("java");
         String string5 = new String("java");
         System.out.println("[TEST 3] string5 == string4: " + String.valueOf(string5 == string4));
+
+        //Easter Egg: To compare the actual values in the reference String, we must use the .equals method.
+        // The comparison operators compare memory locations for reference objects. For primitive variables, the comparison operators compare the actual values
+
+        System.out.println("----------------------------------------------------------------------");
+
+        System.out.println("Comparing the values of string4 and string5");
+        System.out.println("[Easter Egg] Do string4 and string5 have the same values? " + String.valueOf(string4.equals(string5)));
     }
 }
